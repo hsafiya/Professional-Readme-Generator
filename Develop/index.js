63ecf384920cd40ed6fa32c4adacffa483ee3ad1
Safiyya H <safiyya.hasanova@gmail.com>
2021-07-05 19:34:21 -0500

    adding questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,49 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer")
+const fs = require("fs")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    message: 'What is your username?',
+    name: 'github',
+    validate: githubInput => {
+        if (githubInput){
+            return true;
+        } else {
+            console.log ('What is your username?')
+            return false;
+        }
+    }
+},
+    {
+        type: 'input',
+        message: 'What id your email address?',
+        name: 'email',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else{
+                console.log('Please peovide your email');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        message: 'Provide your title',
+        name: 'title',
+        validate: titleInput =>{
+            if (titleInput){
+                return true;
+            } else{
+                console.log('provide your title name');
+                return false;
+            }
+        }
+    },
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
