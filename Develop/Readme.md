ccd68e385f633d185c1416482fda3acf1fe93fb1
Safiyya H <safiyya.hasanova@gmail.com>
2021-07-11 11:14:08 -0500

    fixing badges

---
+++ Develop/Readme.md
@@ -0,0 +1,31 @@
+
+# gsdsgsdg
+[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
+  
+## Description
+gdssdgsd
+  
+## Table of Contents
+  
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+  
+## Installation
+dsggsdg
+## Usage 
+fsdgsdgsf
+## Credits
+sdgdsgsd
+## License
+The Unlicense
+
+## Screenshot
+![alt text](dsgdsgsdg)
+
+## Contact us
+
+GitHub: https://github.com/sgsfsdgs
+
+email:gsdsdgsd  
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,58 +1,53 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if( license === 'Apache License 2.0'){
-    return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
-  } else if ( license === 'GNU AGPLv3'){
-    return`[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
-  } else if (license === 'The Unlicense') {
-  return`[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)` 
-  }     
-}
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
   if (license === 'None'){
     return '';
-  }else{
-renderLicenseBadge(license);
   }
+  else if( license === 'Apache License 2.0'){
+    return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+  `
+  } else if ( license === 'GNU AGPLv3'){
+    return`[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+  `
+  } else if (license === 'The Unlicense') {
+  return`[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
+  ` 
+  }     
 }
-
-
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
-  # ${data.title}
-${renderLicenseLink(data.license)}
-  ## Description
-  ${data.description}
+# ${data.title}
+${renderLicenseBadge(data.license)}
+## Description
+${data.description}
   
-  ## Table of Contents (Optional)
+## Table of Contents
   
-  If your README is very long, add a table of contents to make it easy for users to find what they need.
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
   
-  * [Installation](#installation)
-  * [Usage](#usage)
-  * [Credits](#credits)
-  * [License](#license)
-  
-  ## Installation
-  ${data.installation}
-  ## Usage 
-  ${data.usage}
-  ## Credits
-  ${data.contributors}
-  ## License
-  ${data.license}
-  ## Badges
-  ## Screenshot
-  ![alt text](${data.screenshot})
-  ##Contack us
-  GitHub:https://github.com/${data.github}
-  email:${data.email}
-  
+## Installation
+${data.installation}
+## Usage 
+${data.usage}
+## Credits
+${data.contributors}
+## License
+${data.license}
+
+## Screenshot
+![alt text](${data.screenshot})
+
+## Contact us
+
+GitHub: https://github.com/${data.github}
+
+email:${data.email}  
 `;
 }
 
